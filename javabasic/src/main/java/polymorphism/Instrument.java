6d7dde6dec667966ed0701903d2a250550bc48a2
xitan0506 <xitan0506@gmail.com>
2014-03-26 01:03:59 -0400

    Polymorphism in thinking in Java

---
+++ javabasic/src/main/java/polymorphism/Instrument.java
@@ -0,0 +1,7 @@
+package polymorphism;
+
+class Instrument {
+	public void play(Note n){
+		System.out.println("Instrument.play()");
+	}
+}
---
+++ javabasic/src/main/java/polymorphism/Wind.java
@@ -0,0 +1,8 @@
+package polymorphism;
+
+public class Wind extends Instrument{
+	public void play(Note n){
+		System.out.println("Wind.play() 123" + n);
+	}
+
+}
