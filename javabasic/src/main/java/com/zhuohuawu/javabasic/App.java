437ea7b13afe7c807ea2161d8cd4cba96835d8ef
xitan0506 <xitan0506@gmail.com>
2014-03-26 00:40:37 -0400

    come to the initial

--- javabasic/src/main/java/com/zhuohuawu/javabasic/App.java
+++ javabasic/src/main/java/com/zhuohuawu/javabasic/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Hello World 23!" );
-=======
         System.out.println( "Hello World!" );
->>>>>>> 4f913b01bb8e939556df424b47d858a20079851c
     }
 }
