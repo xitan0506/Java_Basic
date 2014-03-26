4f913b01bb8e939556df424b47d858a20079851c
xitan0506 <xitan0506@gmail.com>
2014-03-25 23:45:08 -0400

    Initial Java maven project

---
+++ javabasic/src/main/java/com/zhuohuawu/javabasic/App.java
@@ -0,0 +1,13 @@
+package com.zhuohuawu.javabasic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
